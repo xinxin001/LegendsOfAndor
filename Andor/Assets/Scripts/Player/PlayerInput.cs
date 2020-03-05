82d0da88851165f0f842e8011a9fc38b43f42f5f
Alex Blostein <alexblostein@alexs-mbp.sf.umontreal.ca>
2020-03-05 12:48:28 -0500

    mouse click and camera following

---
+++ Andor/Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    private float m_h;
+    public float H { get { return m_h; } }
+
+    private float m_v;
+    public float V { get { return m_v; }}
+
+
+
+
+    bool m_inputEnabled = false;
+    public bool InputEnabled
+    {
+        get { return m_inputEnabled; }
+
+        //Here in the setter is where we'll modify the player's energy through the UI
+        set { m_inputEnabled = value; }
+    }
+
+    public void GetKeyInput()
+    {
+        if(m_inputEnabled)
+        {
+            m_h = Input.GetAxisRaw("Horizontal");
+            m_v = Input.GetAxisRaw("Vertical");
+        }
+    }
+
+
+}
---
+++ Andor/Assets/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(PlayerMover))]
+[RequireComponent(typeof(PlayerInput))]
+
+
+public class PlayerManager : MonoBehaviour
+{
+    public PlayerMover playerMover;
+    public PlayerInput playerInput;
+    
+
+
+    private void Awake()
+    {
+        playerMover = GetComponent<PlayerMover>();
+        playerInput = GetComponent<PlayerInput>();
+        playerInput.InputEnabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Ignore player input if user is moving
+        if(playerMover.isMoving)
+        {
+            return;
+        }
+
+        //get Keyboard input
+        playerInput.GetKeyInput();
+
+        if(playerInput.V == 0)
+        {
+            if (playerInput.H < 0) { playerMover.MoveLeft(); }
+            else if (playerInput.H > 0) { playerMover.MoveRight(); }
+        } else if (playerInput. H == 0)
+        {
+            if (playerInput.V < 0)
+            {
+                playerMover.MoveBackward();
+            }else if (playerInput.V > 0)
+            {
+                playerMover.MoveForward();
+            }
+        }
+    }
+}
