0bc4f3e06022501792242c41e7926fdd19e284b3
Oumtata <55467453+Oumtata@users.noreply.github.com>
2020-03-06 20:14:01 -0500

    Add files via upload

---
+++ Assets/Scripts/UI/Rooms/CreateRoomMenu.cs
@@ -0,0 +1,36 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Realtime;
+
+public class CreateRoomMenu : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    private Text _roomName;
+
+    public void OnClick_CreateRoom()
+    {
+        //check if connected before creating a room
+
+        if (!PhotonNetwork.IsConnected)
+            return;
+        //JoinOrCreateRoom
+        RoomOptions options = new RoomOptions();
+        options.MaxPlayers = 4;
+        PhotonNetwork.JoinOrCreateRoom(_roomName.text, options, TypedLobby.Default);
+    }
+
+    public override void OnCreatedRoom()
+    {
+        print("Created room successfully");
+    }
+
+
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        print("Failed to create room." + message);
+    }
+}
---
+++ Assets/Scripts/UI/Rooms/RoomListingsMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class RoomListingsMenu : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    private GameObject _roomListing;
+
+    [SerializeField]
+    private Transform _content;
+
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        
+    }
+
+}
