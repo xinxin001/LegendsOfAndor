932b798e2c41e3003ecde237ffead640e65db42a
Yunke Xiao <yunke0123@hotmail.com>
2020-03-06 19:36:22 -0500

    Add Photon + initial server testing code

---
+++ Assets/Scripts/NetworkManagers/MasterManager.cs
@@ -0,0 +1,18 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Singletons/MasterManager")]
+
+public class MasterManager : SingletonScriptableObject<MasterManager>
+{
+    [SerializeField]
+    private GameSettings _gameSettings;
+    public GameSettings GameSettings{
+        get{
+            return Instance._gameSettings;
+        }
+    }
+}
---
+++ Assets/Scripts/testConnect.cs
@@ -0,0 +1,24 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestConnect : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    private void Start(){
+        print("Connecting to server.");
+        PhotonNetwork.GameVersion = "0.0.1";
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // Update is called once per frame
+    public override void OnConnectedToMaster(){
+        print("Connected to server.");
+    }
+
+    public override void OnDisconnected(DisconnectCause cause){
+        print("Disconnected from server for reason " + cause.ToString());
+    }
+}
