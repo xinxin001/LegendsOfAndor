74dd1b598fb85e2fa9de7660a48efa49ed3700e7
Weiya818 <weiya.zhu@mail.mcgill.ca>
2020-03-09 19:37:40 -0400

    added the well

---
+++ Assets/Scripts/Well/WellClicker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WellClicker : MonoBehaviour
+{
+    [SerializeField] private Material emptyColor;
+    public bool empty = false;
+    //public Color32 emptyColor;
+   
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && empty == false)         {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);             RaycastHit hit;              if (Physics.Raycast(ray, out hit,1000))             {                 var selection = hit.transform;                 var slectionRenderer = selection.GetComponent<Renderer>();
+                 if (selection != null)
+                {
+                    this.GetComponent<Renderer>().material = emptyColor;
+                    empty = true;
+                }
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/Well/WellHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(PolygonCollider2D))]
+
+public class WellHandler : MonoBehaviour
+{
+    public Well well;
+    private SpriteRenderer sprite;
+    public bool change = true;
+
+    public Color32 oldColor;
+    public Color32 hoverColor;
+    public Color32 startColor;
+    public Color32 emptyColor;
+
+    void Awake()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        sprite.color = startColor;
+    }
+    void OnMouseEnter()
+    {
+            oldColor = sprite.color;
+            sprite.color = hoverColor;
+
+    }
+    void OnMouseExit()
+    {
+        sprite.color = startColor;
+        
+    }
+}
