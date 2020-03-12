77f4ec8e47e20d97fad6c060fc0a236c4dd2ef9a
Alex Blostein <alexblostein@Alexs-MacBook-Pro.local>
2020-03-12 00:36:52 -0400

    reitburg castle done

---
+++ Assets/Scripts/Region/ReitBurgCastle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Here all we want to do is to open up the loss meny if Monsters overrun the castle
+
+public class ReitBurgCastle : MonoBehaviour
+{
+    public GameObject lossMenu;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if(this.GetComponent<RegionHandler>().getMonsters().Length > 0)
+        {
+            Pause();
+        }
+    }
+
+    void Pause()
+    {
+        lossMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+}
