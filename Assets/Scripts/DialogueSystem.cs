ed16f81580d646380f9a48e5e4171e2e7822c4e8
Julien Schmidt <julienschmidt.online@gmail.com>
2020-03-11 23:46:11 -0400

    fixed bugs

--- Assets/Scripts/DialogueSystem.cs
+++ Assets/Scripts/DialogueSystem.cs
@@ -8,17 +8,43 @@ public class DialogueSystem : MonoBehaviour
 	
     public static DialogueSystem Instance {get; set;}
     public GameObject dialoguePanel;
+    public GameObject CurrentHero;
     //public GameObject dialoguePanel = GameObject.Find("Dialogue");
     
     Button buySP;
+    Button exitButton;
+    Button buyWineskin;
+    Button buyFalcon;
+    Button buyShield;
+    Button buyBow;
+    Button buyHelm;
+    Button buyTelescope;
    
 
     void Awake(){
-    	//dialoguePanel = GameObject.Find("Canvas/Dialogue");
+
+    	//Get Buttons
     	buySP = dialoguePanel.transform.Find("BuyStrengthPoint").GetComponent<Button>();
-    	//buySP = dialoguePanel.transform.Find("BuyStrengthPoint").GetComponent<Button>();
-    	//buySP = dialoguePanel.transform.GetChild(0).gameObject;
+    	buyWineskin = dialoguePanel.transform.Find("BuyWineskin").GetComponent<Button>();
+    	buyFalcon = dialoguePanel.transform.Find("BuyFalcon").GetComponent<Button>();
+    	buyShield = dialoguePanel.transform.Find("BuyShield").GetComponent<Button>();
+    	buyBow = dialoguePanel.transform.Find("BuyBow").GetComponent<Button>();
+    	buyHelm = dialoguePanel.transform.Find("BuyHelm").GetComponent<Button>();
+    	buyTelescope = dialoguePanel.transform.Find("BuyTelescope").GetComponent<Button>();
+    	exitButton = dialoguePanel.transform.Find("ExitButton").GetComponent<Button>();
+  
+    	
+    	//Listeners
     	buySP.onClick.AddListener(buySPFunc);
+    	buyWineskin.onClick.AddListener(buyWineskinFunc);
+    	buyFalcon.onClick.AddListener(buyFalconFunc);
+    	buyShield.onClick.AddListener(buyShieldFunc);
+    	buyBow.onClick.AddListener(buyBowFunc);
+    	buyHelm.onClick.AddListener(buyHelmFunc);
+    	buyTelescope.onClick.AddListener(buyTelescopeFunc);
+    	exitButton.onClick.AddListener(exitDialogue);
+    	
+
     	dialoguePanel.SetActive(false);
     	
     	
@@ -35,6 +61,37 @@ public class DialogueSystem : MonoBehaviour
 
     void buySPFunc(){
     	Debug.Log("Buy SP");
+    	int currentSP = CurrentHero.getSP();
+    	CurrentHero.setmaxSP(currentSP);
     }
+
+    void buyWineskinFunc(){
+    	Debug.Log("Buy WS");
+    }
+
+    void buyFalconFunc(){
+    	Debug.Log("Buy Falcon");
+    }
+
+    void buyShieldFunc(){
+    	Debug.Log("Buy Shield");
+    }
+
+    void buyBowFunc(){
+    	Debug.Log("Buy Bow");
+    }
+
+    void buyHelmFunc(){
+    	Debug.Log("Buy Helm");
+    }
+
+    void buyTelescopeFunc(){
+    	Debug.Log("Buy Telescope");
+    }
+
+    void exitDialogue(){
+    	dialoguePanel.SetActive(false);
+    }
+    
 
 }
--- Assets/Scripts/Hero/Hero.cs
+++ Assets/Scripts/Hero/Hero.cs
@@ -10,12 +10,22 @@ public class Hero : MonoBehaviour
     private int maxWP;
     private int maxSP;
     private int rN;
+    private int wineskin;
+    private int falcon;
+    private int shield;
+    private int bow;
+    private int helm;
+    private int telescope;
     private HeroKind heroKind;
     public ArrayList farmerlist = new ArrayList();
 
     public int getGold {
         get { return gold; }
     }
+    public void setGold(int g){
+        this.gold = g;
+    }
+
     public int getTime
     {
         get { return timeOfDay; }
@@ -28,11 +38,17 @@ public class Hero : MonoBehaviour
     {
         get { return maxWP; }
     }
+    public void setmaxSP(int sp){
+        this.maxSP = sp;
+    }
     public int getmaxSP{
         get { return maxSP; }
     }
-    public int getrN
-    {
+   
+    public int getSP(){
+        return maxSP;
+    }
+    public int getrN{
         get { return rN; }
     }
     public HeroKind getHerokind{
@@ -50,6 +66,49 @@ public class Hero : MonoBehaviour
 
     public void DropFarmer(Farmer f){
         farmerlist.Remove(f);
+    }
+
+    public int getWineskin{
+        get { return wineskin; }
+    }
+    public void setWineskin(int ws){
+        this.wineskin = ws;
+    }
+
+    public int getFalcon{
+        get { return falcon; }
+    }
+
+    public void setFalcon(int f){
+        this.falcon = f;
+    }
+
+    public int getShield{
+        get { return shield; }
+    }
+    public void setShield(int s){
+        this.shield = s;
+    }
+
+    public int getBow{
+        get { return bow; }
+    }
+    public void setBow(int b){
+        this.bow = b;
+    }
+
+    public int getHelm{
+        get { return helm; }
+    }
+    public void setHelm(int h){
+        this.helm = h;
+    }
+
+    public int getTelescope{
+        get { return telescope; }
+    }
+    public void setTelescope(int t){
+        this.telescope = t;
     }
 
 }
