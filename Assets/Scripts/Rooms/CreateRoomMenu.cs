67bb74175a8c2e7f7bfcefd42ce114adb4011750
Oumtata <55467453+Oumtata@users.noreply.github.com>
2020-03-12 00:10:35 -0400

    Add files via upload

---
+++ Assets/Scripts/Rooms/CreateRoomMenu.cs
@@ -0,0 +1,47 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Realtime;
+
+public class CreateRoomMenu : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    private Text _roomName;
+
+    private RoomsCanvases _roomsCanvases;
+
+    public void FirstInitialize(RoomsCanvases canvases)
+    {
+        _roomsCanvases = canvases;
+    }
+
+    public void OnClick_CreateRoom()
+    {
+        //check if connected before creating a room
+
+        if (!PhotonNetwork.IsConnected){
+            Debug.Log("Not connected to server");
+                return;
+        }
+        print("Creating room.");
+        //JoinOrCreateRoom
+        RoomOptions options = new RoomOptions();
+        options.MaxPlayers = 4;
+        PhotonNetwork.JoinOrCreateRoom(_roomName.text, options, TypedLobby.Default);
+    }
+
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("Created room successfully.", this);
+        _roomsCanvases.CurrentRoomCanvas.Show();
+    }
+
+
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Room creation failed: " + message, this);
+    }
+}
