d9eb9f9f9e9ce380d739a252e6b14d3d61b82b81
Yunke Xiao <33202018+BrotherYunke@users.noreply.github.com>
2020-04-27 16:14:36 -0400

    Change room name format

--- Assets/Scripts/UI/Rooms/RoomListing.cs
+++ Assets/Scripts/UI/Rooms/RoomListing.cs
@@ -13,7 +13,7 @@ public class RoomListing : MonoBehaviour{
 
     public void SetRoomInfo(RoomInfo roomInfo){
         RoomInfo = roomInfo;
-        _text.text = roomInfo.MaxPlayers + ", " + roomInfo.Name;
+        _text.text = roomInfo.Name + " " +roomInfo.PlayerCount + "/" + roomInfo.MaxPlayers;
     }
 
 
