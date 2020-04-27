a91db8adc9a712555e13d09d61f05133b826fe8f
Yunke Xiao <33202018+BrotherYunke@users.noreply.github.com>
2020-04-27 17:39:09 -0400

    Start serialization of Hero

--- Assets/Scripts/Hero/Cursor.cs
+++ Assets/Scripts/Hero/Cursor.cs
@@ -4,86 +4,102 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using CodeMonkey.Utils;
+using Photon.Realtime;
+using Photon.Pun;
 
-public class Cursor : MonoBehaviour
+public class Cursor : MonoBehaviourPunCallbacks
 {
     public Hero hero;
-    
+
     void Update()
     {
         //If the left mouse button is clicked.
         if (Input.GetMouseButtonDown(0))
         {
+            //Get the mouse position on the screen and send a raycast into the game world from that position.
             if (!EventSystem.current.IsPointerOverGameObject())
             {
-                //Get the mouse position on the screen and send a raycast into the game world from that position.
-                Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero);
+                PhotonView photonView = PhotonView.Get(this);
+                photonView.RPC("updatePosition", RpcTarget.All, hero.gameObject);
+            }
+        }
+    }
 
-                //If something was hit, the RaycastHit2D.collider will not be null.
-                if (hit.collider != null)
+    [PunRPC]
+    public void updatePosition(GameObject heroObj)
+    {
+
+        Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero);
+
+        Hero moveHero = heroObj.GetComponent<Hero>();
+
+        //If something was hit, the RaycastHit2D.collider will not be null.
+        if (hit.collider != null)
+        {
+            print(hit.collider.gameObject.name);
+
+            //Hero move
+            if (hit.collider.gameObject.tag == "Region")
+            {
+                if (moveHero.controlPrinceThorald == false)
                 {
-                    print(hit.collider.gameObject.name);
+                    moveHero.move(hit.collider.gameObject);
+                }
+                else
+                {
+                    MonsterManager.princeThorald.move(hit.collider.gameObject);
+                }
 
-                    //Hero move
-                    if (hit.collider.gameObject.tag == "Region")
-                    {
-                        if(hero.controlPrinceThorald == false)
-                        {
-                            hero.move(hit.collider.gameObject);
-                        } else
-                        {
-                            MonsterManager.princeThorald.move(hit.collider.gameObject);
-                        }
-                    
-                    }
-                    //Pickup farmer
-                    else if(hit.collider.gameObject.tag == "Farmer")
-                    {
-                        hero.pickupFarmer(hit.collider.gameObject);
-                    }
-                    //Pickup gold
-                    else if (hit.collider.gameObject.tag == "Gold")
-                    {
-                        hero.pickupGold(hit.collider.gameObject);
+            }
+            //Pickup farmer
+            else if (hit.collider.gameObject.tag == "Farmer")
+            {
+                moveHero.pickupFarmer(hit.collider.gameObject);
+            }
+            //Pickup gold
+            else if (hit.collider.gameObject.tag == "Gold")
+            {
+                moveHero.pickupGold(hit.collider.gameObject);
 
-                    }
-                    //Empty well
-                    else if(hit.collider.gameObject.tag == "Well")
-                    {
-                        GameObject wellRegion = hit.collider.gameObject.GetComponent<Well>().region;
-                        if(hero.isSameRegion(wellRegion)) {
-                            ColorPopup.Create(UtilsClass.GetMouseWorldPosition(), "Well Emptied!", "Green");
-                            hero.emptyWell(hit.collider.gameObject.GetComponent<Well>());
-                        }
-                    } else if (hit.collider.gameObject.tag == "Merchant")
-                    {
-                        print("Select Merchant");
-                        GameObject merchantRegion = hit.collider.gameObject.GetComponent<Merchant>().region;
-                        if (hero.isSameRegion(merchantRegion))
-                        {
-                            hero.interactMerchant(hit.collider.gameObject.GetComponent<Merchant>());
+            }
+            //Empty well
+            else if (hit.collider.gameObject.tag == "Well")
+            {
+                GameObject wellRegion = hit.collider.gameObject.GetComponent<Well>().region;
+                if (moveHero.isSameRegion(wellRegion))
+                {
+                    ColorPopup.Create(UtilsClass.GetMouseWorldPosition(), "Well Emptied!", "Green");
+                    moveHero.emptyWell(hit.collider.gameObject.GetComponent<Well>());
+                }
+            }
+            else if (hit.collider.gameObject.tag == "Merchant")
+            {
+                print("Select Merchant");
+                GameObject merchantRegion = hit.collider.gameObject.GetComponent<Merchant>().region;
+                if (moveHero.isSameRegion(merchantRegion))
+                {
+                    moveHero.interactMerchant(hit.collider.gameObject.GetComponent<Merchant>());
 
-                        }
-                    } else if (hit.collider.gameObject.tag == "Witch")
-                    {
-                        print("Select Witch");
-                        GameObject witchRegion = hit.collider.gameObject.GetComponent<Witch>().region;
-                        if (hero.isSameRegion(witchRegion))
-                        {
-                            hero.interactWitch(hit.collider.gameObject.GetComponent<Witch>());
+                }
+            }
+            else if (hit.collider.gameObject.tag == "Witch")
+            {
+                print("Select Witch");
+                GameObject witchRegion = hit.collider.gameObject.GetComponent<Witch>().region;
+                if (moveHero.isSameRegion(witchRegion))
+                {
+                    moveHero.interactWitch(hit.collider.gameObject.GetComponent<Witch>());
 
-                        }
-                    }
-                    else if (hit.collider.gameObject.tag == "Fog")
-                    {
-                        GameObject fogRegion = hit.collider.gameObject.GetComponent<Fog>().region;
-                        if (hero.isSameRegion(fogRegion) && !hit.collider.gameObject.GetComponent<Fog>().isUsed)
-                        {
-                            ColorPopup.Create(UtilsClass.GetMouseWorldPosition(), "Fog", "Green");
-                            hero.interactFog(hit.collider.gameObject.GetComponent<Fog>());
-                        }
-                    }
+                }
+            }
+            else if (hit.collider.gameObject.tag == "Fog")
+            {
+                GameObject fogRegion = hit.collider.gameObject.GetComponent<Fog>().region;
+                if (moveHero.isSameRegion(fogRegion) && !hit.collider.gameObject.GetComponent<Fog>().isUsed)
+                {
+                    ColorPopup.Create(UtilsClass.GetMouseWorldPosition(), "Fog", "Green");
+                    moveHero.interactFog(hit.collider.gameObject.GetComponent<Fog>());
                 }
             }
         }
