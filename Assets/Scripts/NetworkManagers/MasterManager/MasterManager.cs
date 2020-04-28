bec56c58f671963efcdd0858f3def46a356e3113
Yunke Xiao <33202018+BrotherYunke@users.noreply.github.com>
2020-04-27 22:08:08 -0400

    Remove redundant code

--- Assets/Scripts/NetworkManagers/MasterManager/MasterManager.cs
+++ Assets/Scripts/NetworkManagers/MasterManager/MasterManager.cs
@@ -55,22 +55,22 @@ public class MasterManager : SingletonScriptableObject<MasterManager>
     private static void PopulateNetworkedPrefabs()
     {
 #if UNITY_EDITOR
-        Instance._networkedPrefabs.Clear();
+        //Instance._networkedPrefabs.Clear();
 
-        GameObject[] results = Resources.LoadAll<GameObject>("");
-        for (int i = 0; i < results.Length; i++)
-        {
-            if (results[i].GetComponent<PhotonView>() != null)
-            {
-                string path = AssetDatabase.GetAssetPath(results[i]);
-                Instance._networkedPrefabs.Add(new NetworkedPrefab(results[i], path));
-            }
-        }
-        //DEBUGGING PURPOSES
-        for (int i = 0; i < Instance._networkedPrefabs.Count; i++)
-        {
-            UnityEngine.Debug.Log(Instance._networkedPrefabs[i].Prefab.name + ", " + Instance._networkedPrefabs[i].Path);
-        }
+        //GameObject[] results = Resources.LoadAll<GameObject>("");
+        //for (int i = 0; i < results.Length; i++)
+        //{
+        //    if (results[i].GetComponent<PhotonView>() != null)
+        //    {
+        //        string path = AssetDatabase.GetAssetPath(results[i]);
+        //        Instance._networkedPrefabs.Add(new NetworkedPrefab(results[i], path));
+        //    }
+        //}
+        ////DEBUGGING PURPOSES
+        //for (int i = 0; i < Instance._networkedPrefabs.Count; i++)
+        //{
+        //    UnityEngine.Debug.Log(Instance._networkedPrefabs[i].Prefab.name + ", " + Instance._networkedPrefabs[i].Path);
+        //}
 
 #endif
     }
